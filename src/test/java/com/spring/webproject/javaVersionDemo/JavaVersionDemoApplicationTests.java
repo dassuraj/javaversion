3622b447d920dcc89fc641920f4e2c9b68595ec0
Rajesh das <okayjourney@gmail.com>
2024-05-03 09:38:20 +0545

    java version project

---
+++ src/test/java/com/spring/webproject/javaVersionDemo/JavaVersionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.webproject.javaVersionDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaVersionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
